93b20e2fcbba200f03b36b8bf0a201a733c41d28
Arler <arler2w@gmail.com>
2026-06-28 16:39:47 +0300

    Добавлена модель Cart в админку django

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Book, BookLoan
+from .models import Book, BookLoan, Cart
 # Register your models here.
 
 class BookLoanAdmin(admin.ModelAdmin):
 	list_display = ('loan_date', 'return_date')
 
 
+class CartAdmin(admin.ModelAdmin):
+	filter_horizontal = ['books']
+
+
 admin.site.register(Book)
 admin.site.register(BookLoan, BookLoanAdmin)
+admin.site.register(Cart, CartAdmin)
